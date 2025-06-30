486f6e5a0c15a7859ada45649115bbd4e2f70e94
Josiane Garcia <josimachadogarcia@gmail.com>
2025-06-30 00:05:52 -0300

    Ajustes

--- livraria-josiane/src/main/java/com/livraria/livraria/model/repository/EditoraRepository.java
+++ livraria-josiane/src/main/java/com/livraria/livraria/model/repository/EditoraRepository.java
@@ -4,8 +4,6 @@ import com.livraria.livraria.model.domain.Editora;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface EditoraRepository extends JpaRepository<Editora, Long> {
     
